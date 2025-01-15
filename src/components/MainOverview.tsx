12dbba4e40c455dff9d5ae9a1dbeb55b708aa237
Odin <odin.thorsen@gmail.com>
2025-01-15 21:38:37 +0100

    some improvements

--- src/components/MainOverview.tsx
+++ src/components/MainOverview.tsx
@@ -1,21 +1,13 @@
 import {useState} from "react";
 import {WorkerSchedule} from "./WorkerSchedule.tsx";
-import {getEmptyMainCalendar} from "../utils.ts";
+import {getEmptyMainCalendar, getEmptyWorkerCalendar} from "../utils.ts";
 
 const names: Name[] = ["John Doe", "Max Waldo", "Ashton Kutcher", "Brad Pitt", "Angelina Jolie", "Tom Cruise", "temp1", "temp2", "temp3"];
 
-const deliveryTimes: Record<Day, Timeslot | undefined> = {
-    Monday: undefined,
-    Tuesday: "08-00-12:00",
-    Wednesday: undefined,
-    Thursday: "12-00-16:00",
-    Friday: undefined,
-    Saturday: "12-00-16:00",
-    Sunday: undefined,
-};
 
 export function MainOverview() {
     const [calendar, setCalendar] = useState(getEmptyMainCalendar());
+    const [deliveryTimes, setDeliveryTimes] = useState(getEmptyWorkerCalendar());
 
     // @ts-expect-error - keys will return a days array
     const days: Day[] = Object.keys(calendar);
@@ -27,16 +19,29 @@ export function MainOverview() {
     <div>
         <h1>Main Overview</h1>
         <table>
-            <tr>
-                <th>Time of day</th>
-                {days.map((day) => <th key={day}>{day}</th>)}
-            </tr>
-            {timeslots.map((timeslot) => <tr>
-                <td><div>{timeslot}</div></td>
-                {days.map(day => <td style={{
-                    backgroundColor: deliveryTimes[day] === timeslot ? "orange" : "",
-                }} key={day + timeslot}><div >{calendar[day][timeslot].map(name => <p>{name}</p>)}</div></td>)}
-            </tr>)}
+            <thead>
+                <tr>
+                    <th>Time of day</th>
+                    {days.map((day) => <th key={day}>{day}</th>)}
+                </tr>
+            </thead>
+            <tbody>
+                {timeslots.map((timeslot) => <tr>
+                    <td><div>{timeslot}</div></td>
+                    {days.map(day => <td onClick={e => {
+                        e.preventDefault()
+                        setDeliveryTimes(prev => ({
+                            ...prev,
+                            [day]: {
+                                ...prev[day],
+                                [timeslot]: !prev[day][timeslot]
+                            }
+                        }))
+                    }} style={{
+                        backgroundColor: deliveryTimes[day][timeslot] ? "orange" : "",
+                    }} key={day + timeslot}><div >{calendar[day][timeslot].map(name => <p>{name}</p>)}</div></td>)}
+                </tr>)}
+            </tbody>
         </table>
         <br/>
         <h2>Individual worker Schedules</h2>
--- src/components/WorkerSchedule.tsx
+++ src/components/WorkerSchedule.tsx
@@ -21,37 +21,40 @@ export function WorkerSchedule(p: {
             padding: "5px",
         }}>Hours allocated: {hoursAllocated}</b>
         <table>
-            <tr>
-                <th>Time of day</th>
-                {days.map((day) => <th key={day}>{day}</th>)}
-            </tr>
-            {timeslots.map((timeslot) => <tr>
-                <td>{timeslot}</td>
-                {days.map(day => <td key={day + timeslot}
-                                     onClick={(e) => {
-                                         e.preventDefault();
-                                         const isSigningUp = !personalCalendar[day][timeslot]
-                                         setHoursAllocated(prevState => prevState + (isSigningUp ? 4 : -4))
-                                         setPersonalCalendar(prev => ({
-                                             ...prev,
-                                             [day]: {...prev[day], [timeslot]: !prev[day][timeslot]}
-                                         }))
-                                         p.setMainCalendar(prev => ({
-                                             ...prev,
-                                             [day]: {
-                                                 ...prev[day],
-                                                 [timeslot]: isSigningUp ? [...prev[day][timeslot], p.name] : prev[day][timeslot].filter(name => name !== p.name)
-                                             }
-                                         }))
-                                     }}
-                                     style={{
-                                         backgroundColor: personalCalendar[day][timeslot] ? "green" : "",
-                                         minHeight: "60px",
-                                         minWidth: "100px",
-                                         cursor: "pointer",
-                                         userSelect: "none",
-                }}>{personalCalendar[day][timeslot] ? "Working" : "Not Working"}</td>)}
-            </tr>)}
+            <thead>
+                <tr>
+                    <th>Time of day</th>
+                    {days.map((day) => <th key={day}>{day}</th>)}
+                </tr>
+            </thead>
+            <tbody>
+                {timeslots.map((timeslot) => <tr>
+                    <td>{timeslot}</td>
+                    {days.map(day => <td key={day + timeslot}
+                                         onClick={(e) => {
+                                             e.preventDefault();
+                                             const isSigningUp = !personalCalendar[day][timeslot]
+                                             setHoursAllocated(prevState => prevState + (isSigningUp ? 4 : -4))
+                                             setPersonalCalendar(prev => ({
+                                                 ...prev,
+                                                 [day]: {...prev[day], [timeslot]: !prev[day][timeslot]}
+                                             }))
+                                             p.setMainCalendar(prev => ({
+                                                 ...prev,
+                                                 [day]: {
+                                                     ...prev[day],
+                                                     [timeslot]: isSigningUp ? [...prev[day][timeslot], p.name] : prev[day][timeslot].filter(name => name !== p.name)
+                                                 }
+                                             }))
+                                         }}
+                                         style={{
+                                             backgroundColor: personalCalendar[day][timeslot] ? "green" : "",
+                                             minHeight: "60px",
+                                             minWidth: "100px",
+
+                    }}>{personalCalendar[day][timeslot] ? "Working" : "Not Working"}</td>)}
+                </tr>)}
+            </tbody>
         </table>
     </div>
 }
